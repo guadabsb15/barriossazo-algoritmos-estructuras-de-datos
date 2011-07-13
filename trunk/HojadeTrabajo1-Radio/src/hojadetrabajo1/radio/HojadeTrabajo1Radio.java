056014da16c0faa1130e13f31152048ad9c8080b
guadabsb15 <guadabsb15@86c1af44-48f8-a26b-2891-dc2ae9dfe0bd>
2011-07-13 01:47:49 +0000

    Version1

---
+++ trunk/HojadeTrabajo1-Radio/src/hojadetrabajo1/radio/HojadeTrabajo1Radio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hojadetrabajo1.radio;
+
+/**
+ *
+ * @author BarriosSazo
+ */
+public class HojadeTrabajo1Radio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
