ad0622bc3f9c2a163c2ec41606ee5777099ae912
guadabsb15 <guadabsb15@86c1af44-48f8-a26b-2891-dc2ae9dfe0bd>
2011-07-13 19:23:45 +0000

    Version2

--- trunk/HojadeTrabajo1-Radio/src/hojadetrabajo1/radio/HojadeTrabajo1Radio.java
+++ trunk/HojadeTrabajo1-Radio/src/hojadetrabajo1/radio/HojadeTrabajo1Radio.java
@@ -3,17 +3,29 @@
  * and open the template in the editor.
  */
 package hojadetrabajo1.radio;
-
+import java.util.*;
 /**
  *
  * @author BarriosSazo
  */
 public class HojadeTrabajo1Radio {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        System.out.println("Menu:");
+        System.out.println("1. Prende el radio");
+        System.out.println("2. Cambia de AM a FM");
+        System.out.println("3. Avanzar en el dial de las emisoras");
+        System.out.println("4. Guardar una emisora en uno de los 12 botones");
+        System.out.println("5. Permite seleccionar la emisora puesta en un botón");
+        System.out.println("6. Apagar el radio");
+        
+        int i = s.nextInt();
+        
+        System.out.println(i);
     }
 }
