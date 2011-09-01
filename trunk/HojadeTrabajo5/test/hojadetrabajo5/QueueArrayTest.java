9d739905c343cf2b24ab349320b06c92617e90ec
guadabsb15@gmail.com <guadabsb15@gmail.com@86c1af44-48f8-a26b-2891-dc2ae9dfe0bd>
2011-09-01 10:47:36 +0000

    version 2 con tests

---
+++ trunk/HojadeTrabajo5/test/hojadetrabajo5/QueueArrayTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hojadetrabajo5;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author BarriosSazo
+ */
+public class QueueArrayTest extends TestCase {
+    
+    public QueueArrayTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of offer method, of class QueueArray.
+     */
+    public void testOffer() {
+        System.out.println("offer");
+        Object value = 4;
+        QueueArray instance = new QueueArray(5);//crea cola que puede tener 5 elementos
+        boolean expResult = true;
+        boolean result = instance.offer(value);
+        assertEquals(expResult, result);
+        assertEquals(4, instance.peek());
+      
+    }
+
+    /**
+     * Test of poll method, of class QueueArray.
+     */
+    public void testPoll() {
+        System.out.println("poll");
+        QueueArray instance = new QueueArray(5);//crea cola que puede tener 5 elementos
+        instance.add(4);
+        instance.add(3);
+        instance.add(1);
+        Object result = instance.poll();
+        Object expResult = 4;
+        assertEquals(expResult, result);
+        assertEquals(3, instance.peek());
+    }
+
+    /**
+     * Test of peek method, of class QueueArray.
+     */
+    public void testPeek() {
+        System.out.println("peek");
+        QueueArray instance = new QueueArray(5);//crea cola que puede tener 5 elementos
+        instance.add(4);
+        instance.add(3);
+        instance.add(1);
+        Object expResult = 4;
+        Object result = instance.peek();
+        assertEquals(expResult, result);
+        
+    }
+}
