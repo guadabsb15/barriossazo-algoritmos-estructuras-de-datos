0684e9017acf97ffe9cf1e43b710094f2a45e5d0
guadabsb15@gmail.com <guadabsb15@gmail.com@86c1af44-48f8-a26b-2891-dc2ae9dfe0bd>
2011-09-06 18:19:07 +0000

    version 5 terminada

--- trunk/HojadeTrabajo5/src/hojadetrabajo5/QueueList.java
+++ trunk/HojadeTrabajo5/src/hojadetrabajo5/QueueList.java
@@ -1,6 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Para hacer la cola implementada con array y la cola implementada con 
+ * lista circular, se uso como referencia el libro
+ * Java Structures de Duane Bailey
  */
 package hojadetrabajo5;
 
